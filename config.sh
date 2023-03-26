387efa407503b7ebddf4d29e98ff1b5fa0552561
UnknownShakib <124501955+UnknownShakib@users.noreply.github.com>
2023-03-26 15:16:56 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -18,6 +18,6 @@ export OUTPUT="PBRP*.zip"
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
 export USE_CCACHE=1
-export CCACHE_SIZE="50G"
+export CCACHE_SIZE="60G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
